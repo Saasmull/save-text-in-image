97ca238c79aa08ee070d97ce23d6cd35b283c38b
Saasmull <79657472+Saasmull@users.noreply.github.com>
2021-09-25 21:07:31 +0200

    Add files via upload

---
+++ files/read.py
@@ -0,0 +1,12 @@
+with open("photo.jpg", "rb")as f:
+    print("State                                          | Output")
+    print("-----------------------------------------------+------------------------------------------")
+    print("Opening File...                                |")
+    content = f.read()
+    print("Searching the end of the JPG code format...    |")
+    offset = content.index(bytes.fromhex("FFD9"))
+    f.seek(offset + 2)
+    print("Reading the text...                            |")
+
+    print("                                               | The text is: " + str(f.read()))
+    print("Done                                           |")
